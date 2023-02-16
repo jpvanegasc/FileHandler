7f861659807d2c0fa505a6ec4968a941e99daebe
Juan Pablo Vanegas <jpvanegasc@gmail.com>
2023-02-16 08:36:45 -0500

    :sparkles: Add load and dum methods

--- src/json_handler.cpp
+++ src/json_handler.cpp
@@ -18,3 +18,42 @@ void FileHandler::write_file(std::string filename) {
     file << content;
     file.close();
 }
+
+void JsonHandler::dumps(std::map<std::string, std::string> json) {
+    content = "{\n";
+    for (const auto& [key, value] : json) {
+        content += "    \"" + key + "\": \"" + value + "\",\n";
+    }
+    content[content.length() - 2] = ' ';
+    content += "}";
+}
+
+void JsonHandler::dump(std::map<std::string, std::string> json, std::string filename) {
+    dumps(json);
+    write_file(filename);
+}
+
+void JsonHandler::loads(std::string string) {
+    std::string key, value;
+    short quote_position = 0;
+
+    for (int i = 0; i < string.length(); i++) {
+        char c = string[i];
+        if (c == '"') {
+            quote_position = (quote_position + 1) % 5;
+            continue;
+        }
+
+        if (quote_position == 1)
+            key += c;
+        else if (quote_position == 2)
+            // values between key and value (i.e. whitespace and '"')
+            continue;
+        else if (quote_position == 3)
+            value += c;
+        else if (quote_position == 4) {
+            json[key] = value;
+            quote_position = (quote_position + 1) % 5;
+        }
+    }
+}
--- src/json_handler.h
+++ src/json_handler.h
@@ -38,4 +38,21 @@ class FileHandler {
     void write_file(std::string filename);
 };
 
-#endif  // __FILE_HANDLER_H
+/**
+ * Read and write a json file
+ *
+ * @param filename: name or path of file.
+ * @param delim_char : file data delimiter. Default is ','.
+ * @param comm_char : file comment indicator. Default is '#'
+ */
+class JsonHandler : public FileHandler {
+   public:
+   std::map<std::string, std::string> json;
+    void dumps(std::map<std::string, std::string> json);
+    void dump(std::map<std::string, std::string> json, std::string filename);
+
+    void loads(std::string string);
+    void load(std::string filename);
+};
+
+#endif  // __JSON_HANDLER_H
